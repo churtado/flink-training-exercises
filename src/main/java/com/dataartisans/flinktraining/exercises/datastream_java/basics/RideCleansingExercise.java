01d5696a89062d5538f618b16a92a72b44719bb7
minjeonglim <oij>
2018-12-01 19:18:57 +0000

    asdf

--- src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/basics/RideCleansingExercise.java
+++ src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/basics/RideCleansingExercise.java
@@ -19,11 +19,13 @@ package com.dataartisans.flinktraining.exercises.datastream_java.basics;
 import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
 import com.dataartisans.flinktraining.exercises.datastream_java.utils.ExerciseBase;
+import com.dataartisans.flinktraining.exercises.datastream_java.utils.GeoUtils;
 import com.dataartisans.flinktraining.exercises.datastream_java.utils.MissingSolutionException;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamUtils;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -41,20 +43,29 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class RideCleansingExercise extends ExerciseBase {
 	public static void main(String[] args) throws Exception {
 
-//		ParameterTool params = ParameterTool.fromArgs(args);
-//		final String input = params.get("input", ExerciseBase.pathToRideData);
-//
-//		final int maxEventDelay = 60;       // events are out of order by max 60 seconds
-//		final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
-//
-//		// set up streaming execution environment
-//		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-//		env.setParallelism(ExerciseBase.parallelism);
-//
-//		// start the data generator
-//		DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(new TaxiRideSource(input, maxEventDelay, servingSpeedFactor)));
-//
+		ParameterTool params = ParameterTool.fromArgs(args);
+		final String input = params.get("input", ExerciseBase.pathToRideData);
+
+		final int maxEventDelay = 60;       // events are out of order by max 60 seconds
+		final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
+
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		env.setParallelism(ExerciseBase.parallelism);
+
+		DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(new TaxiRideSource(input, maxEventDelay, servingSpeedFactor)));
+
+		DataStream<TaxiRide> filteredRides = rides.filter(new FilterFunction<TaxiRide>() {
+			@Override
+			public boolean filter(TaxiRide taxiRide) throws Exception {
+				return GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat) &&
+						GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
+			}
+		});
+
+		filteredRides.print();
+		env.execute("filtered rides");
+
 //		DataStream<TaxiRide> filteredRides = rides
 //				// filter out rides that do not start or stop in NYC
 //				.filter(new NYCFilter());
